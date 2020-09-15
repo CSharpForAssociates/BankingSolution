af1e968d938ea401ac5e57bc1756307b496fcac4
Jeffry Gonzalez <jeff@hypertheory.com>
2020-09-15 15:10:25 -0400

    done with that misadventure

---
+++ BankingUnitTests/GoldAccountDeposits.cs
@@ -0,0 +1,26 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingUnitTests
+{
+    public class GoldAccountDeposits
+    {
+        [Fact]
+        public void GoldAccountsGetBonus()
+        {
+            var account = new GoldAccount();
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 100M;
+
+            account.Deposit(amountToDeposit);
+
+            Assert.Equal(
+                openingBalance + amountToDeposit + 10M,
+                account.GetBalance());
+            
+        }
+    }
+}
