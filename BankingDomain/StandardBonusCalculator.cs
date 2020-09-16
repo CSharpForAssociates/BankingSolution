b47db45fdb97feb5b5c8bc695076748c5051129d
Jeffry Gonzalez <jeff@hypertheory.com>
2020-09-16 13:44:57 -0400

    before refactor

---
+++ BankingDomain/StandardBonusCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingDomain
+{
+    public class StandardBonusCalculator : ICalculateBankAccountBonuses
+    {
+        public decimal GetDepositBonusFor(decimal balance, decimal amountToDeposit)
+        {
+
+            if (EligibleForBonus(balance))
+            {
+                if (BeforeCutoff())
+                {
+                    return amountToDeposit * .10M;
+                }
+                else
+                {
+                    return amountToDeposit * .08M;
+                }
+            }
+            return 0;
+        }
+
+        protected virtual bool BeforeCutoff()
+        {
+            return DateTime.Now.Hour < 17;
+        }
+
+        private bool EligibleForBonus(decimal balance)
+        {
+            return balance >= 1000;
+        }
+    }
+}
---
+++ BankingUnitTests/StandardBonusCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingUnitTests
+{
+    public class StandardBonusCalculatorTests
+    {
+        // Balances below 1000 never get a bonus (either before or after the cutoff) (2)
+        // Balances at or over 1000 
+        //      - Before Cutoff get 10%
+        //      - After Cutoff get 8%
+
+        [Fact]
+        public void BonusBeforeCutoff()
+        {
+            var calculator = new BeforeCutoffBonusCalculator();
+            var bonus = calculator.GetDepositBonusFor(1000, 100);
+            Assert.Equal(10, bonus);
+        }
+
+        [Fact]
+        public void BonusAfterCutoff()
+        {
+            var calculator = new AfterCutoffBonusCalculator();
+            var bonus = calculator.GetDepositBonusFor(1000, 100);
+            Assert.Equal(8, bonus);
+        }
+
+    }
+
+    public class BeforeCutoffBonusCalculator : StandardBonusCalculator
+    {
+
+        protected override bool BeforeCutoff()
+        {
+            return true;
+        }
+    }
+    public class AfterCutoffBonusCalculator : StandardBonusCalculator
+    {
+        protected override bool BeforeCutoff()
+        {
+            return false;
+        }
+    }
+}
